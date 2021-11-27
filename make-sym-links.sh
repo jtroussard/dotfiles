a27b3f3d7354aa9e7956470a783f3252bdeac4c5
jacques <jacques.troussard@gmail.com>
2021-11-26 19:29:15 -0500

    fixed broken sym links

--- make-sym-links.sh
+++ make-sym-links.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired Dotfiles in ~/Dotfiles
-############################
-
-########## Variables
-
-dir=~/Dotfiles                    # Dotfiles directory
-olddir=~/Dotfiles_old             # old Dotfiles backup directory
-files="bashrc vimrc vim profile"    # list of files/folders to symlink in homedir
-
-##########
-
-# create Dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing Dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the Dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing Dotfiles in homedir to Dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing Dotfiles from ~ to $olddir"
-    mv ~/.$file ~/Dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+# This script overwrites existing dot files!
+shopt -s dotglob
+DIR="${HOME}/dotfiles/target_home"
+cd $DIR
+for name in *; do
+	echo "linking ${name}"
+	ln -sfT $DIR/$name $HOME/$name
 done
